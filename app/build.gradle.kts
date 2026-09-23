df7250afa9e4895036e42a9398898742b9c6e593
MILAD_TEHRAN <13803495+miladtak@users.noreply.github.com>
2026-09-23 08:57:14 +0330

    Raise compileSdk and targetSdk to 36 for Media3 1.11.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     namespace = "com.miladtak.japo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.miladtak.japo"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
     }
