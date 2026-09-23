aa28eb6c8072ae6a51543442c1f0376b3e38a1b3
MILAD_TEHRAN <13803495+miladtak@users.noreply.github.com>
2026-09-23 14:44:59 +0330

    Define real bitmap video encoder contract

--- app/src/main/java/com/miladtak/japo/encoder/VideoEncoder.kt
+++ app/src/main/java/com/miladtak/japo/encoder/VideoEncoder.kt
@@ -1,6 +1,8 @@
 package com.miladtak.japo.encoder
 
 interface VideoEncoder {
-    fun start(output: String)
+    fun start(output: String, width: Int, height: Int, frameRate: Int = 30)
+    fun encode(frame: android.graphics.Bitmap, presentationTimeUs: Long)
     fun stop()
 }
+
