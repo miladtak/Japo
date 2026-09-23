227d7605449ec4f2fea7ab3916ccf3785ed68d00
MILAD_TEHRAN <13803495+miladtak@users.noreply.github.com>
2026-09-23 09:01:52 +0330

    Align Java and Kotlin JVM targets to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,11 @@ plugins {
 
 android {
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
     namespace = "com.miladtak.japo"
     compileSdk = 36
