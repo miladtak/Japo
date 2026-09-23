b16a7821ca4e891b6ad86e854ddb34b6bde2fa86
MILAD_TEHRAN <13803495+miladtak@users.noreply.github.com>
2026-09-23 13:25:01 +0330

    Import color utilities for blur compositor

--- app/src/main/java/com/miladtak/japo/matting/BackgroundReplacer.kt
+++ app/src/main/java/com/miladtak/japo/matting/BackgroundReplacer.kt
@@ -2,6 +2,7 @@ package com.miladtak.japo.matting
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 
